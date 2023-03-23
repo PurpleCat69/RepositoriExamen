0380b4122f9a4489bd264e2cc17e4f2c9b7005c8
PurpleCat69 <127231132+PurpleCat69@users.noreply.github.com>
2023-03-23 18:55:47 +0100

    Create Examen.java

---
+++ Examen.java
@@ -0,0 +1,6 @@
+public class Examen {
+        public static void main(String[] args) {
+          String xxx = "Nour Aissioui";
+          System.out.println("Soc "+xxx+" i estic fent l’examen de la UF2 del M01.");
+        }
+    }
